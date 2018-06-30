f828bfed18fbee54df327899508fa51cc0621406
masa <jpeezzy@hotmail.com>
2018-06-29 18:06:33 -0700

    grab csv data from website

--- nbaStat.py
+++ nbaStat.py
@@ -6,6 +6,9 @@ class MyHTMLParser(HTMLParser):
         #super().__init__()
         HTMLParser.__init__(self)
         self.tag_on = 0
+        self.name_on = 0
+        self.count = 0
+        self.date = 0
         return
     def handle_starttag(self, tag, attrs):
         if(tag == 'pre'):
@@ -13,13 +16,15 @@ class MyHTMLParser(HTMLParser):
         if any('csv_pgl_basic' in s for s in attrs):
             self.tag_on = 1;
             print("encountered csv Table:")
-        #if any("class" in s for s in attrs):
-            #if any("class" in s for s in attrs):
-            #print(attrs)
-        #    if any('overthrow table_container'in s for s in attrs):
+        #find name 
+        if(tag == "strong" and self.count <3):
+            if(self.count == 0):
+                self.date = 1 #to get the year of play
+            self.count += 1
+            if(self.count == 3):
+                self.name_on = 1
         return
         
-        #print "Encountered a start tag:", tag
 
     def handle_endtag(self, tag):
         if(tag == 'pre'):
@@ -29,8 +34,22 @@ class MyHTMLParser(HTMLParser):
         #print "Encountered an end tag :", tag
 
     def handle_data(self, data):
+        if(self.date == 1):
+            self.year = data
+            #print(self.year)
+            self.date = 0
         if( self.tag_on == 1):
-            print "Encountered some data  :", data
+            self.f.write(data)
+            self.f.close()
+            #print "Encountered some data  :", data
+            return
+        if(self.name_on == 1):
+            #write to file
+            fname = data+self.year+".csv"
+            fname = fname.replace(" " , "_")
+            print(fname)
+            self.f= open(fname,"w+")
+            self.name_on = 0
             return
 
 # instantiate the parser and fed it some HTML

8cc644a2508631de973365f25b82d3a22b8bc689
masa <jpeezzy@hotmail.com>
2018-07-05 00:11:51 -0700

    added alphabetical order

--- nba.js
+++ nba.js
@@ -1,32 +1,49 @@
-//console.log("HELLO");
-/*var page = require('webpage').create();
+var page = require('webpage').create();
 var fs = require('fs');
 
-var url = "https://www.basketball-reference.com/players/j/jordami01/gamelog/1986/";
-page.open(url, function(status) {
-	console.log("Status: " + status);
-	if(status === "success") {
-		var title = page.evaluate( "table2csv(\"pgl_basic\")");
-		fs.write('1985.html', page.content, 'w');
+var iteratePage = function(letter){
+	if(letter == '{')
+	{
+		phantom.exit();
+		return;
 	}
-	phantom.exit();
-});*/
-
-
-	var page = require('webpage').create();
-	var fs = require('fs');
+	//Iterating through the alphabet
+	//console.log(String.fromCharCode(letter.charCodeAt() + i));
+	var address = "https://www.basketball-reference.com/players/" + String.fromCharCode(letter.charCodeAt());
+	console.log(address);
+	t = Date.now();
+	page.open(address, function(status) {
+		if (status !== 'success') {
+			console.log('FAIL to load the address');
+		} 
+		else {
+			t = Date.now() - t;
+			console.log('Loading ' + address);
+			console.log('Loading time ' + t + ' msec');
+			//FIND NAMES OF PLAYERS
+			//var title = page.evaluate( "table2csv(\"pgl_basic\")");
+			var fn =  "Alphabetical_Order/"+letter + '.html';
+			fs.write(fn, page.content, 'w');
+		}
+		iteratePage( String.fromCharCode(letter.charCodeAt()+1));
+	});
+	//end iteration
+}
 var loadpage = function (i, max){
 	if (i === max) {
 		phantom.exit();
 		return;
 	}
+
+
 	var address = "https://www.basketball-reference.com/players/j/jordami01/gamelog/" + i.toString();
 	//var address = "https://www.basketball-reference.com/players/j/jordami01/gamelog/" + i.toString() + '/';
 	t = Date.now();
 	page.open(address, function(status) {
 		if (status !== 'success') {
 			console.log('FAIL to load the address');
-		} else {
+		} 
+		else {
 			t = Date.now() - t;
 			console.log('Loading ' + address);
 			console.log('Loading time ' + t + ' msec');
@@ -39,21 +56,5 @@ var loadpage = function (i, max){
 	});
 };
 
-loadpage(1985, 2004);
-/*
-for(var i = 1985; i <= 2003; i++)
-{
-	var page = require('webpage').create();
-	var fs = require('fs');
-	var url = "https://www.basketball-reference.com/players/j/jordami01/gamelog/" + i.toString() + "/";
-//console.log(url);
-	page.open(url, function(status) {
-		console.log("Status: " + status);
-		if(status === "success") {
-			var title = page.evaluate( "table2csv(\"pgl_basic\")");
-			var fn = i.toString() + '.html';
-			fs.write(fn, page.content, 'w');
-		}
-	phantom.exit();
-	});
-}*/
+//loadpage(1985, 2004);
+iteratePage('a');
--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-
-
+console.log("HELLO TEST")
+var letter = 'a';
+for(var i = 0; i < 26; i++)
+{
+	console.log(String.fromCharCode(letter.charCodeAt() + i));
+}
 var page = require('webpage').create();
 page.open('http://example.com', function(status) {
 	  console.log("Status: " + status);
